43b35acda963611e3da4ea03f5238e4e550c9f6e
Braul-oss <bhmejia2@gmail.com>
2024-11-22 12:56:32 -0600

    Nuevos cambios en página nosotros

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -4,10 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/about.css') }}">
+    <link href="{{ asset('css/about.css') }}" rel="stylesheet">
 
 </head>
 <body>
+
+<nav class="navbar">
+        <div class="logo-container">
+            <a href="login.html"> 
+                <img src="{{ asset('imagenes/dino.jpg') }}" alt="Logo" class="logo1">
+            </a>
+                <img src="{{ asset('imagenes/logolt.png') }}" alt="Logo2" class="logo2">
+        </div>
+        <ul>
+            <li><a href="{{ route('login') }}">Iniciar sesión</a></li>
+            <li><a href="{{ route('productos') }}">Productos</a></li>
+            <li><a href="{{ route('servicios') }}">Servicios</a></li>
+            <li><a href="{{ route('nosotros') }}">Nosotros</a></li>
+        </ul>
+    </nav>
 
 <div class="about-page">
     <section class="hero-section">
@@ -22,7 +37,7 @@
         <div class="values-grid">
             <div class="value-item">
                 <h3>Nuestra Misión</h3>
-                <p>Conectar personas y refrescar el mundo de manera sustentable.</p>
+                <p>Ser una empresa de desarrollo de software reconocida.</p>
             </div>
             <div class="value-item">
                 <h3>Nuestra Visión</h3>
@@ -38,14 +53,37 @@
     <section class="history-section">
         <h2>Nuestra Historia</h2>
         <p>
-            Desde nuestros humildes comienzos hasta convertirnos en una de las empresas más importantes del mundo,
-            nuestra historia está llena de hitos que reflejan nuestro compromiso con la innovación y la excelencia.
+            Desde nuestros humildes comienzos somos una empresa que nace a partir
+            de un proyecto escolar.
         </p>
         <p>
-            Hoy seguimos escribiendo esta historia con pasión, dedicación y un compromiso firme con nuestros
-            consumidores y el planeta.
+            Hoy seguimos desarrollando otros proyectos escolares y buscamos algún dia 
+            ser reconocidos como una empresa importante.
         </p>
     </section>
+
+    <section class="team-section">
+    <h2>Nuestro Equipo Directivo</h2>
+    <p>Conoce a las personas que lideran nuestra organización con visión y compromiso.</p>
+    <div class="team-grid">
+        <div class="team-member">
+            <img src="{{ asset('imagenes/Azalia.jpg') }}" alt="Director General">
+            <h3>Azalia Mejía</h3>
+            <p>Directora General</p>
+        </div>
+        <div class="team-member">
+            <img src="{{ asset('imagenes/Gustavo.jpg') }}" alt="Directora Financiera">
+            <h3>Gustavo Vazquez</h3>
+            <p>Desarrollador web</p>
+        </div>
+        <div class="team-member">
+            <img src="{{ asset('imagenes/img.png') }}" alt="Director de Operaciones">
+            <h3>Braulio Hernández</h3>
+            <p>Gestor de bases de datos</p>
+        </div>
+    </div>
+</section>
+
 </div>
 </body>
 </html>
